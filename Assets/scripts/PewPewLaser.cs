ae87fbacb9772e6912cd28fa02a18a334656f1f3
Kevin Jayamanna <mercury0110111@gmail.com>
2017-01-21 09:59:23 -0800

    added things na na na

---
+++ Assets/scripts/PewPewLaser.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+* This is the stereotype ship laser
+* It simply moves in a straight line and
+* damages whatever it hits
+*/
+public class PewPewLaser : Particle
+{
+    // Use this for initialization
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!active)
+            return;
+        handleLifetime();
+    }
+}
