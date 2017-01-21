ffbf76d5d38a5703b22cc2656e05dbda2fb909a8
Kevin Jayamanna <mercury0110111@gmail.com>
2017-01-20 20:11:40 -0800

    moveable mc and some interfaces

---
+++ Assets/scripts/MainController.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+using System;
+
+// Script to accept user input and move a given object appropriately
+// Eg, when you press up or right, this script is what captures that
+// and delivers the relevant commands to the MovementObj possessed by
+// this object
+public class MainController : MonoBehaviour, ControllerIntf {
+    public bool inactive;
+    private string objectName = "Player";
+    private MovementObj obj;
+
+    // Use this for initialization
+    void Start()
+    {
+        obj = GetComponent<MovementObj>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        if (vertical != 0)
+        {
+            obj.move(vertical);
+        }
+        else
+        {
+            obj.brake();
+        }
+
+        if (horizontal != 0)
+        {
+            obj.rotate(horizontal);
+        }
+    }
+
+    // The player is of the Player faction
+    NocturneDefinitions.Faction ControllerIntf.getFaction()
+    {
+        return NocturneDefinitions.Faction.Player;
+    }
+
+    // the player's name is just "Player"
+    string ControllerIntf.getName()
+    {
+        return objectName;
+    }
+
+
+    // the player never has a set target
+    GameObject ControllerIntf.getTarget()
+    {
+        return null;
+    }
+
+    // you cannot change the affiliation of the player
+    void ControllerIntf.setFaction(NocturneDefinitions.Faction faction)
+    {
+        return;
+    }
+
+    // the player cannot have a set target
+    void ControllerIntf.setTarget()
+    {
+        return;
+    }
+
+    void ControllerIntf.pause()
+    {
+        throw new NotImplementedException();
+    }
+
+
+    void ControllerIntf.unpause()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Assets/scripts/MovementObj.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// This is the component that interprets commands to move the entity
+// Eg, if asked to "move" by the Controller of this entity, this 
+// script will take that command and move the entity accordingly
+public class MovementObj : MonoBehaviour {
+    public float moveSpeed = 1;
+    public float rotationSpeed = 5;
+
+    // Use this for initialization
+    void Start () {
+        gameObject.AddComponent<Rigidbody2D>();
+        gameObject.GetComponent<Rigidbody2D>().gravityScale = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void brake()
+    {
+        Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+        rigidbody.velocity = rigidbody.velocity * (float)0.40;
+    }
+
+    public void move(float vertical)
+    {
+        Vector2 temp = new Vector2(Mathf.Cos(getAngle()),
+   Mathf.Sin(getAngle()));
+        Rigidbody2D rigidbody = gameObject.GetComponent<Rigidbody2D>();
+        if (vertical != 0)
+            rigidbody.velocity = temp * moveSpeed * (vertical + Mathf.Sign(vertical));
+        temp = Vector3.zero;
+
+        /*
+        if (vertical > 0)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+            GetComponent<Animator>().Play("Active");
+        }
+        else if (vertical < 0)
+        {
+            GetComponent<SpriteRenderer>().enabled = true;
+            GetComponent<Animator>().Play("Reverse");
+        }
+        */
+    }
+
+    public void rotate(float horizontal)
+    {
+        Vector3 temp = new Vector3(0, 0, -1 * rotationSpeed * horizontal);
+        if (horizontal != 0)
+            gameObject.transform.Rotate(temp);
+        temp = Vector3.zero;
+    }
+
+    public float getAngle()
+    {
+        return (transform.rotation.eulerAngles.z + 90) * Mathf.PI / 180;
+    }
+}
