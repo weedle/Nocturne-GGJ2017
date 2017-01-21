e9dc4c322708e1f899401114daf194a15b2e0074
Jenne H <iv.castaire@gmail.com>
2017-01-21 12:59:22 -0800

    Just game logic

---
+++ Assets/scripts/GameLogic.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class GameLogic : MonoBehaviour {
+	public Button button;
+	public Color testColor = Color.white;
+	public Color gameColor = Color.black;
+	private bool visible;
+
+	// Use this for initialization
+	void Start () {
+		visible = false;
+		button.onClick.AddListener (makeVisible);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void makeVisible() {
+		GameObject[] wallList = GameObject.FindGameObjectsWithTag ("Wall");
+
+		foreach (GameObject wall in wallList) {
+			SpriteRenderer thisWall = wall.GetComponent<SpriteRenderer> ();
+			if (visible) {
+				thisWall.color = gameColor;
+			} else {
+				thisWall.color = testColor;
+			}
+		}
+
+		visible = !visible;
+	}
+}
