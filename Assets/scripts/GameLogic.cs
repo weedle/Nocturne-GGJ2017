8b3aa3be21fc4dd3bdedf7fbeb9fd263bbdeebd9
Kevin Jayamanna <mercury0110111@gmail.com>
2017-01-25 01:08:25 -0800

    Colors are now user-configurable in the option menu!

--- Assets/scripts/GameLogic.cs
+++ Assets/scripts/GameLogic.cs
@@ -6,6 +6,7 @@ public class GameLogic : MonoBehaviour {
 	public Button button;
 	public Color testColor = Color.white;
 	public Color gameColor = Color.black;
+    public bool usePreferredColor = false;
 	private bool visible = true;
 
     public bool gotCollectible1 = false;
@@ -20,9 +21,33 @@ public class GameLogic : MonoBehaviour {
     // Use this for initialization
     void Start ()
     {
+        Color camColor = new Color(gameColor.r / 2.05f, gameColor.g / 2.05f, gameColor.b / 2.05f);
+
+        if (usePreferredColor)
+        {
+            int i = UnityEngine.PlayerPrefs.GetInt("colorPreset");
+            switch (i)
+            {
+                case 0:
+                    gameColor = Color.black;
+                    camColor = Color.black;
+                    break;
+                case 1:
+                    gameColor = new Color(42f / 255f, 0, 96f / 255f);
+                    camColor = new Color(19f / 255f, 0, 45f / 255f);
+                    break;
+                case 2:
+                    gameColor = new Color(0, 20f / 255f, 85f / 255f);
+                    camColor = new Color(0, 10f / 255f, 40f / 255f);
+                    break;
+                case 3:
+                    gameColor = new Color(2f / 255f, 69f / 255f, 0);
+                    camColor = new Color(3.5f / 255f, 34.5f / 255f, 0);
+                    break;
+            }
+        }
         switchVisible ();
 		button.onClick.AddListener (switchVisible);
-        Color camColor = new Color(gameColor.r/2.1f, gameColor.g/2.1f, gameColor.b/2.1f);
         GameObject.Find("Main Camera").GetComponent<Camera>()
             .backgroundColor = camColor;
     }
--- Assets/scripts/UIlogic.cs
+++ Assets/scripts/UIlogic.cs
@@ -3,18 +3,91 @@ using System.Collections;
 using UnityEngine.SceneManagement;
 
 public class UIlogic : MonoBehaviour {
+    string prefix = "COL PRESET:";
 
-    // Use this for initialization
-    void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    void Start()
+    {
 
-	public void loadScene(int index) {
+        int i = UnityEngine.PlayerPrefs.GetInt("colorPreset");
+        Color gameColor = getColor(i);
+        GameObject obj = GameObject.Find("colorButton");
+        if(obj != null)
+        {
+            obj.GetComponentInChildren<UnityEngine.UI.Text>().text
+                = prefix + i;
+            UnityEngine.UI.ColorBlock blk;
+            blk = obj.GetComponentInChildren<UnityEngine.UI.Button>().colors;
+            blk.normalColor = gameColor;
+            obj.GetComponentInChildren<UnityEngine.UI.Button>().colors = blk;
+        }
+    }
+
+    public void loadScene(int index) {
 		UnityEngine.SceneManagement.SceneManager.LoadScene (index);
 	}
+
+    public void setColor()
+    {
+        int i = UnityEngine.PlayerPrefs.GetInt("colorPreset");
+        switch (i)
+        {
+            case 0:
+                i = 1;
+                GameObject.Find("colorButton").
+                    GetComponentInChildren<UnityEngine.UI.Text>().text
+                    = prefix + i;
+                break;
+            case 1:
+                i = 2;
+                GameObject.Find("colorButton").
+                    GetComponentInChildren<UnityEngine.UI.Text>().text
+                    = prefix + i;
+                break;
+            case 2:
+                i = 3;
+                GameObject.Find("colorButton").
+                    GetComponentInChildren<UnityEngine.UI.Text>().text
+                    = prefix + i;
+                break;
+            case 3:
+                i = 0;
+                GameObject.Find("colorButton").
+                    GetComponentInChildren<UnityEngine.UI.Text>().text
+                    = prefix + i;
+                break;
+            default:
+                i = 0;
+                break;
+        }
+
+        Color gameColor = getColor(i);
+        UnityEngine.PlayerPrefs.SetInt("colorPreset", i);
+        GameObject obj = GameObject.Find("colorButton");
+        UnityEngine.UI.ColorBlock blk;
+        blk = obj.GetComponentInChildren<UnityEngine.UI.Button>().colors;
+        blk.normalColor = gameColor;
+        blk.highlightedColor = gameColor;
+        obj.GetComponentInChildren<UnityEngine.UI.Button>().colors = blk;
+    }
+
+    private Color getColor(int i)
+    {
+        Color gameColor = Color.black;
+        switch (i)
+        {
+            case 0:
+                gameColor = Color.black;
+                break;
+            case 1:
+                gameColor = new Color(42f / 255f, 0, 96f / 255f);
+                break;
+            case 2:
+                gameColor = new Color(0, 20f / 255f, 85f / 255f);
+                break;
+            case 3:
+                gameColor = new Color(2f / 255f, 69f / 255f, 0);
+                break;
+        }
+        return gameColor;
+    }
 }
